1a8d25ffd6961c6482fc24f808070f48ab80deed
Saurabh Mirajkar <mirajkar46@gmail.com>
2020-05-18 18:51:03 +0530

    Lesson 4 - Deal with Hex Colors

--- GetItDone/TDGradient.swift
+++ GetItDone/TDGradient.swift
@@ -15,13 +15,18 @@ class TDGradient: UIView {
         UIColor(red: 58, green: 123, blue: 213).cgColor
     ]
     
+    let hexColors : [CGColor] = [
+        UIColor(rgb: 0x64E4FF).cgColor,  // #64E4FF
+        UIColor(rgb: 0x3A7BD5).cgColor   // #3A7BD5
+    ]
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.translatesAutoresizingMaskIntoConstraints = false
         
         if let layer = self.layer as? CAGradientLayer {
-            layer.colors = colors
+            layer.colors = self.hexColors
             layer.locations = [0.0, 1.2]
         }
         
